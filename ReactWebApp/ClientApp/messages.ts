d4b83439c1976a4333e6bc36d82f72726381c138
joseph dillon <joseph.dillon@vercoglobal.com>
2018-04-03 21:14:31 +0100

    Add project files.

---
+++ ReactWebApp/ClientApp/messages.ts
@@ -0,0 +1,11 @@
+﻿
+//export interface IMessage : { } ????
+
+export default {
+    'en-GB': {
+        'testcomponent.testkey': 'this is a resourced string'
+    },
+    'fr-FR': {
+        'testcomponent.testkey': 'this is a resourced string'
+    },
+}
